90f5c2c19eb7387a0d847759231b6d4e50aa3595
Your Name <you@example.com>
2018-03-24 14:16:09 +0000

    Production update

--- thepursuit/settings/production.py
+++ thepursuit/settings/production.py
@@ -4,7 +4,7 @@ from thepursuit.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','35.154.85.209','thepursuit.ingeniushack.com','tiusrupeht.ingeniushack.com']
+ALLOWED_HOSTS = ['127.0.0.1','13.126.226.206','thepursuit.tedxpesitbsc.com','tiusrupeht.ingeniushack.com']
 
 #ALLOWED_HOSTS += [os.environ['DJANGO_HOST_NAME']]
 
@@ -15,8 +15,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'thepursuit',                 # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'ingenius',
-        'PASSWORD': 'inGenius',
+        'USER': 'tedx',
+        'PASSWORD': 'tedx',
         'HOST': 'localhost',                 # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
